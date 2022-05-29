0bfea6fc76027c89a335c110fc440c88189c294a
Diego Carmona Bernal <cbdiegox@gmail.com>
2022-05-29 15:25:06 -0500

    update

--- user/update.php
+++ user/update.php
@@ -1,54 +1,49 @@
 <?php
 include_once '../modules/security.php';
 include_once '../modules/conexion.php';
+include_once '../modules/notif_info_msgbox.php';
 
 require_once($_SESSION['raiz'] . '/modules/sections/role-access-admin-student.php');
 
-if (empty($_POST['txtuseridUpdate'])) {
+$_POST['txtuserid'] = trim($_POST['txtuserid']);
+
+if (empty($_POST['txtuserid'])) {
 	header('Location: /');
 	exit();
-} else {
-	$pass = mysqli_real_escape_string($conexion, $_POST['txtuserpassOldUpdate']);
+}
+if ($_POST['txtuserid'] == '') {
+	Error('Ingrese un ID de usuario correcto.');
+	header('Location: /user');
+	exit();
+}
 
-	$sql = "SELECT * FROM users WHERE user = '" . $_POST['txtuseridUpdate'] . "' and BINARY pass = '" . $pass . "' LIMIT 1";
+if ($_SESSION['permissions'] == 'admin') {
+	$sql = "SELECT * FROM administratives WHERE user = '" . $_POST['txtuserid'] . "'";
+} else if ($_SESSION['permissions'] == 'student') {
+	$sql = "SELECT * FROM students WHERE user = '" . $_POST['txtuserid'] . "'";
+}
 
-	if ($result = $conexion->query($sql)) {
-		if ($row = mysqli_fetch_array($result)) {
-			$sql = "SELECT user, email FROM users WHERE email = '" . $_POST['txtemailupdate'] . "' AND user != '" . $_POST['txtuseridUpdate'] . "' LIMIT 1";
+if ($result = $conexion->query($sql)) {
+	if ($row = mysqli_fetch_array($result)) {
+		$date = date('Y-m-d H:i:s');
 
-			if ($result = $conexion->query($sql)) {
-				if ($row = mysqli_fetch_array($result)) {
-					$_SESSION['msgbox_info'] = 0;
-					$_SESSION['msgbox_error'] = 1;
-					$_SESSION['text_msgbox_error'] = 'Este correo electrónico ya está en uso.';
-					header('Location: /user');
-					exit();
-				} else {
-					if ($_POST['txtuserpassNewUpdate'] == $_POST['txtuserpassConfirmUpdate'] and $_POST['txtuserpassNewUpdate'] != "" and $_POST['txtuserpassConfirmUpdate'] != "") {
-						$sql_update = "UPDATE users SET email = '" . $_POST['txtemailupdate'] . "', pass = '" . $_POST['txtuserpassNewUpdate'] . "' WHERE user = '" . $_POST['txtuseridUpdate'] . "'";
-					} else {
-						$sql_update = "UPDATE users SET email = '" . $_POST['txtemailupdate'] . "' WHERE user = '" . $_POST['txtuseridUpdate'] . "'";
-					}
-
-					if (mysqli_query($conexion, $sql_update)) {
-						$_SESSION['msgbox_error'] = 0;
-						$_SESSION['msgbox_info'] = 1;
-						$_SESSION['text_msgbox_info'] = 'Usuario actualizado.';
-					} else {
-						$_SESSION['msgbox_info'] = 0;
-						$_SESSION['msgbox_error'] = 1;
-						$_SESSION['text_msgbox_error'] = 'Error al actualizar.';
-					}
-					header('Location: /user');
-					exit();
-				}
-			}
-		} else {
-			$_SESSION['msgbox_info'] = 0;
-			$_SESSION['msgbox_error'] = 1;
-			$_SESSION['text_msgbox_error'] = 'Contraseña incorrecta.';
-			header('Location: /user');
-			exit();
+		if ($_SESSION['permissions'] == 'admin') {
+			$sql_update = "UPDATE administratives SET name = '" . trim($_POST['txtname']) . "', surnames = '" . trim($_POST['txtsurnames']) . "', curp = '" . trim($_POST['txtcurp']) . "', rfc = '" . trim($_POST['txtrfc']) . "', date_of_birth = '" . trim($_POST['dateofbirth']) . "', gender = '" . trim($_POST['selectgender']) . "', phone = '" . trim($_POST['txtphone']) . "', address = '" . trim($_POST['txtaddress']) . "', level_studies = '" . trim($_POST['selectlevelstudies']) . "', occupation = '" . trim($_POST['txtoccupation']) . "', observations = '" . trim($_POST['txtobservation']) . "', updated_at = '" . $date . "' WHERE user = '" . trim($_POST['txtuserid']) . "'";
+		} else if ($_SESSION['permissions'] == 'student') {
+			$sql_update = "UPDATE students SET name = '" . trim($_POST['txtname']) . "', surnames = '" . trim($_POST['txtsurnames']) . "', curp = '" . trim($_POST['txtcurp']) . "', rfc = '" . trim($_POST['txtrfc']) . "', date_of_birth = '" . trim($_POST['dateofbirth']) . "', gender = '" . trim($_POST['selectGender']) . "', phone = '" . trim($_POST['txtphone']) . "', address = '" . trim($_POST['txtaddress']) . "', career = '" . trim($_POST['selectCareer']) . "', documentation = '" . trim($_POST['selectDocumentation']) . "', admission_date = '" . trim($_POST['dateadmission']) . "', updated_at = '" . $date . "' WHERE user = '" . trim($_POST['txtuserid']) . "'";
 		}
+
+		if (mysqli_query($conexion, $sql_update)) {
+			Info('Usuario actualizado.');
+		} else {
+			Error('Error al actualizar usuario.');
+		}
+
+		header('Location: /user');
+		exit();
+	} else {
+		Error('Este ID de usuario no existe.');
+		header('Location: /user');
+		exit();
 	}
 }
